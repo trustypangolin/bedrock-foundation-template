504bb692cae3184c88eb744cc5a2d1849292ec84
Benjamin Luton <benjamin@luton.net.au>
2025-08-28 14:42:40 +1000

    iam

--- tf/org/iam-tf-role.tf
+++ tf/org/iam-tf-role.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "bedrock" {
   name                 = "bedrock-terraform"
   assume_role_policy   = data.aws_iam_policy_document.bedrock_trust.json
-  managed_policy_arns  = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  # managed_policy_arns  = ["arn:aws:iam::aws:policy/AdministratorAccess"]
   max_session_duration = 43200
 }
 
